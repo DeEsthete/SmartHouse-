3026597268efaf4762f89af6b883d306c31113c3
JekaLion <djoni-01@mail.ru>
2017-12-07 17:49:01 +0600

    struct done

--- SmartHousLibrary/StructSensorInformation.cs
+++ SmartHousLibrary/StructSensorInformation.cs
@@ -6,8 +6,19 @@ using System.Threading.Tasks;
 
 namespace SmartHousLibrary
 {
-    class StructSensorInformation
+    public struct StructSensorInformation
     {
+        public double Temperature { get; set; }
+        public bool IsMooving { get; set; }
+        public bool IsOpen { get; set; }
+        public int Watt { get; set; }
 
+        public StructSensorInformation(int temperature, bool isMooving, bool isOpen, int watt)
+        {
+            Temperature = temperature;
+            IsMooving = isMooving;
+            IsOpen = isOpen;
+            Watt = watt;
+        }
     }
 }
---
+++ SmartHousLibrary/UserOptions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartHousLibrary
+{
+    public class UserOptions
+    {
+        public double TemperatureLimit { get; set; }
+        public int WattLimit { get; set; }
+        public UserOptions(int temperature, int watt)
+        {
+            TemperatureLimit = temperature;
+            WattLimit = watt;
+        }
+    }
+}
