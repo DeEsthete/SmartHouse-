bf00e8a5dcc28f1c59c0bd53a0b239c92fbbfa47
UkiOnna <sikirovtemirlan@mail.ru>
2017-12-22 17:39:30 +0600

    Asas

--- SmartHousLibrary/Logic.cs
+++ SmartHousLibrary/Logic.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.IO.Ports;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,19 +13,52 @@ namespace SmartHousLibrary
     {
         //SerialPort serialPort1 = new SerialPort("serialPort1", 9600);
 
-        public void Ardu(bool on)
-        {
-            SerialPort port = new SerialPort("COM6", 9600);
-            port.Open();
-            if (on == true)
+        //public void Ardu(bool on)
+        //{
+        //    SerialPort port = new SerialPort("COM6", 9600);
+        //    port.Open();
+        //    if (on == true)
+        //    {
+        //        port.WriteLine("1");
+        //    }
+        //    else
+        //    {
+        //        port.WriteLine("2");
+        //    }
+        //    port.Close();
+        //}
+
+        public  void Ardu(bool isOn)
+        { 
+            WebRequest request;
+            if (isOn)
             {
-                port.WriteLine("1");
+                request = WebRequest.Create(
+                 "http://10.3.6.80/$1");
             }
             else
             {
-                port.WriteLine("2");
+                 request = WebRequest.Create(
+                    "http://10.3.6.80/$2");
+
             }
-            port.Close();
+            // If required by the server, set the credentials.
+            request.Credentials = CredentialCache.DefaultCredentials;
+            // Get the response.
+            WebResponse response = request.GetResponse();
+            // Display the status.
+            Console.WriteLine(((HttpWebResponse)response).StatusDescription);
+            // Get the stream containing content returned by the server.
+            Stream dataStream = response.GetResponseStream();
+            // Open the stream using a StreamReader for easy access.
+            StreamReader reader = new StreamReader(dataStream);
+            // Read the content.
+            string responseFromServer = reader.ReadToEnd();
+            // Display the content.
+            Console.WriteLine(responseFromServer);
+            // Clean up the streams and the response.
+            reader.Close();
+            response.Close();
         }
     }
 }
